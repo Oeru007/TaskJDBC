c9bc22b9a2b3841dd51871b2a1dfbe23d54356cf
Oeru007 <Oeru007@mail.ru>
2021-10-18 19:25:21 +0300

    Final commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -52,16 +52,16 @@ public class UserDaoJDBCImpl implements UserDao {
             if (conn != null) {
                 try {
                     conn.rollback();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                } catch (SQLException ignore) {
+
                 }
             }
         } finally {
             if (conn != null) {
                 try {
                     conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                } catch (SQLException ignore) {
+
                 }
             }
         }
@@ -80,8 +80,8 @@ public class UserDaoJDBCImpl implements UserDao {
             if (conn != null) {
                 try {
                     conn.rollback();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                } catch (SQLException ignore) {
+
                 }
             }
         } finally {
