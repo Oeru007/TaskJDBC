8d14e114ad642220bad2332c05ee85cf7c39b1c3
Oeru007 <Oeru007@mail.ru>
2021-10-21 18:16:46 +0300

    Revert "hibernate commit"
    This reverts commit 104fca278913c710e9b05245826a50825fdb5661.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,6 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -26,32 +22,12 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Transaction transaction = null;
-        User user = new User(name, lastName, age);
-        try (Session session = Util.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.save(user);
-            transaction.commit();
-        } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
+
     }
 
     @Override
     public void removeUserById(long id) {
-        Transaction transaction = null;
-        try (Session session = Util.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            session.delete(id);
-            transaction.commit();
-        } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        }
+
     }
 
     @Override
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,22 +1,10 @@
 package jm.task.core.jdbc.util;
 
-import jm.task.core.jdbc.model.User;
-import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class Util {
-    private static SessionFactory sessionFactory;
-
     public static Connection getMySQLConnection() throws SQLException {
         String hostName = "localhost";
         String dbName = "jmpp113";
@@ -33,37 +21,5 @@ public class Util {
         Connection connection = DriverManager.getConnection(connectionURL, userName, password);
         connection.setAutoCommit(false);
         return connection;
-    }
-
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
-
-                Properties properties = new Properties();
-                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/jmpp113");
-                properties.put(Environment.USER, "root");
-                properties.put(Environment.PASS, "1346798250Alen");
-                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-
-                properties.put(Environment.SHOW_SQL, "true");
-
-                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-
-                properties.put(Environment.HBM2DDL_AUTO, "validate");
-
-                configuration.setProperties(properties);
-                configuration.addAnnotatedClass(User.class);
-                sessionFactory = configuration.buildSessionFactory();
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties())
-                        .build();
-            } catch (HibernateException e) {
-                e.printStackTrace();
-            }
-        }
-        return sessionFactory;
-
     }
 }
